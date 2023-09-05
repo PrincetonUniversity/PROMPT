451412640f76fa533bfe822ba35ae45b32bb9c95
Ziyang Xu <ziyangx@princeton.edu>
2023-09-05 16:29:23 -0400

    Update cmake files and format castutil

--- src/lib/CastUtil.h
+++ src/lib/CastUtil.h
@@ -1,28 +1,26 @@
 #ifndef CAST_UTIL_H
 #define CAST_UTIL_H
 
-#include <vector>
-#include "llvm/IR/Instruction.h"
 #include "scaf/Utilities/InstInsertPt.h"
+#include "llvm/IR/Instruction.h"
+#include <vector>
 
 namespace liberty {
 
-  typedef std::vector<llvm::Instruction*> NewInstructions;
+typedef std::vector<llvm::Instruction *> NewInstructions;
 
-  llvm::Value *castToInt64Ty(llvm::Value *value,
+llvm::Value *castToInt64Ty(llvm::Value *value, liberty::InstInsertPt &out,
+                           NewInstructions *newInstructions = 0);
+
+llvm::Value *castIntToInt32Ty(llvm::Value *value, liberty::InstInsertPt &out,
+                              NewInstructions *newInstructions = 0);
+
+llvm::Value *castFromInt64Ty(llvm::Type *ty, llvm::Value *value,
                              liberty::InstInsertPt &out,
                              NewInstructions *newInstructions = 0);
 
-  llvm::Value *castIntToInt32Ty(llvm::Value *value,
-                             liberty::InstInsertPt &out,
-                             NewInstructions *newInstructions = 0);
-
-  llvm::Value *castFromInt64Ty(llvm::Type *ty, llvm::Value *value,
-                               liberty::InstInsertPt &out,
-                               NewInstructions *newInstructions = 0);
-
-  llvm::Value *castPtrToVoidPtr(llvm::Value *value, liberty::InstInsertPt &out,
-                               NewInstructions *newInstructions = 0);
-}
+llvm::Value *castPtrToVoidPtr(llvm::Value *value, liberty::InstInsertPt &out,
+                              NewInstructions *newInstructions = 0);
+} // namespace liberty
 
 #endif /* CAST_UTIL_H */
